dabe5bb26ed098dad3741edd7c355c64d4a5c885
wond <wond@gmail.com>
2026-08-10 10:13:50 +0900

    애플리케이션 진입점, jpa auditing 설정

---
+++ src/main/java/com/liveklass/enrollment/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.liveklass.enrollment.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
